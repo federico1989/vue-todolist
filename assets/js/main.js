f7cec97fe3ee329ffa846da32dde4672beb8caba
Federico <federico.scarpati1989@gmail.com>
2020-12-17 11:09:52 +0100

    Utilizzo del v-if
    Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,10 @@ let app = new Vue ({
   el: "#my_list",
   data: {
     image: "assets/img/logo.png",
+    // L'utente puó inserire nuove tasks
     message: "",
+    // La nostra todo list avrá alcune tasks di default predefinite
+    noTask:"Nulla da fare",
     tasks: [
       "Do something",
       "Do else",
@@ -13,8 +16,12 @@ let app = new Vue ({
     addNewTask: function () {
       this.tasks.push(this.message);
     },
+    // Cliccando sulla "X" l'utente puó cancellare una task
     erase: function(index) {
       this.tasks.splice(index, 1);
     }
   }
 })
+// Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
+// Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
+// Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
